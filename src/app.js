af760e7912d421697c90e3b787788e29c195beff
Stepan <kurakste@gmail.com>
2019-06-28 21:33:40 +0300

    fix game loop

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import imgLoader from './helpers/imgLoader';
 
-function App(name) {
+function App(name, gameSpeed) {
   // [{name, <img>}, ...] 
   this.spriteSheets = []
   this.name = name;
-
+  this.gameSpeed = gameSpeed;
+  this.work = true;
+  console.log('from init', this.gameSpeed);
+  this.clock = () => {
+    now = Date.now();
+    let dt = (now-lt);
+    if (dt <= this.gameSpeed*1000) return;
+    lt = Date.now();
+    console.log('tic', dt);
+  }
 }
+let now = Date.now();
+let lt = Date.now();
 /**
  * arrOfSpritesheets is:
  *  [
@@ -16,8 +27,8 @@ function App(name) {
 App.prototype.loadSpritesSheets = async function (arrOfSpritesheets) {
   const promBound = arrOfSpritesheets.map(img => imgLoader(img.src));
   const data = await Promise.all(promBound);
-  const out  = arrOfSpritesheets.map((el, i) => {
-    return {name: el.name, img: data[i]}
+  const out = arrOfSpritesheets.map((el, i) => {
+    return { name: el.name, img: data[i] }
   });
   this.spriteSheets = out;
 }
@@ -25,6 +36,7 @@ App.prototype.loadSpritesSheets = async function (arrOfSpritesheets) {
 App.prototype.loadGameMap = function (arrOfActors) {
 
 }
+
 
 App.prototype.init = function () {
   console.log('I start well!', this);
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import sprSheetsArray from './img/imgSpritesheetsArr';
 
 
 (async function () {
-  const app = new App('Tanks');
+  const app = new App('Tanks', 1/24);
   await app.loadSpritesSheets(sprSheetsArray);
   app.init();
+  setInterval(app.clock, 8)
 
 })();
