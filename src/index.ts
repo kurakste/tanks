7e6941b2d30eb2a2c19d96b8fa13c1ee1f8e807f
Stepan <kurakste@gmail.com>
2019-06-30 16:15:46 +0300

    some clean up

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import Tank from './obects/Tank';
 
 (async function () {
   const tank = new Tank(20, 30, 5, []);
-  console.log('tank created: ', tank.subsctiptions);
+  console.log('tank created: ', tank);
 
   const tanks = new Tanks('Tanks', 1);
   await tanks.loadSpritesSheets(sprSheetsArray);
