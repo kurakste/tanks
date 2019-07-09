decdc947f7502bca3289e090b50453206d6d6956
Stepan <kurakste@gmail.com>
2019-07-09 22:58:47 +0300

    add ball maps

--- src/obects/Tank/pictureMap.js
+++ src/obects/Tank/pictureMap.js
@@ -62,6 +62,22 @@ const fire = [
   { x: 0 + 2 * 32, y: 0 + 0 * 32, width: 30, height: 30, }, 
 ]
 
+ballLeft  = [
+  { x: 0 + 5 * 32, y: 0 + 0 * 32, width: 30, height: 30, }, 
+]
+
+ballRight  = [
+  { x: 0 + 4 * 32, y: 0 + 0 * 32, width: 30, height: 30, }, 
+]
+
+ballUp  = [
+  { x: 0 + 6 * 32, y: 0 + 0 * 32, width: 30, height: 30, }, 
+]
+
+ballDown  = [
+  { x: 0 + 3 * 32, y: 0 + 0 * 32, width: 30, height: 30, }, 
+]
+
 export {
   tankDown, tankLeft, tankRight, tankUp,
   tankGoDown, tankGoLeft, tankGoRight, tankGoUp,
