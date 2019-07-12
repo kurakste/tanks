224d2197b641e38ce2f9bcae7d40d9abd64c1593
Stepan <kurakste@gmail.com>
2019-07-12 22:06:21 +0300

    add sprites for brick

--- src/obects/Brick/index.ts
+++ src/obects/Brick/index.ts
@@ -2,7 +2,7 @@ import Actor from "../actores/Actor";
 import Sprites from '../../interfaces/Sprites';
 import Sprite from "../actores/Sprites";
 import subsctiptions from '../../interfaces/enum/Subscriptions';
-import { brick } from './pictureMap';
+import { brick, brick70, brick40, brick10 } from './pictureMap';
 import Game from "../../interfaces/Games";
 import Hittable from "../../interfaces/Hittable";
 
@@ -12,21 +12,26 @@ export default class Brick extends Actor implements Hittable {
   game: Game;
   tick: number;
   health: number;
+  maxHealth: number;
 
   constructor(xpos: number, ypos: number, game: Game) {
 
-    const brickSpr = new Sprite(
-      "fullSpriteSheet", brick, [0]
-    );
+    const brickSpr = new Sprite("fullSpriteSheet", brick, [0]);
+    const brickSpr70 = new Sprite("fullSpriteSheet", brick70, [0]);
+    const brickSpr40 = new Sprite("fullSpriteSheet", brick40, [0]);
+    const brickSpr10 = new Sprite("fullSpriteSheet", brick10, [0]);
 
 
-    super(xpos, ypos, 3, [brickSpr]);
+    super(xpos, ypos, 3, [brickSpr, brickSpr70, brickSpr40, brickSpr10]);
     this.subsctiptions.push(subsctiptions.hits);
     this.activeSprite = brickSpr;
     this.game = game;
     this.tick = 0;
     this.health = 100;
+    this.maxHealth =100;
   }
+
+
 
   clock() {
     (this.health<=0) && this.game.removeFigure(this);
@@ -35,6 +40,13 @@ export default class Brick extends Actor implements Hittable {
 
   getsHit(damage: number) {
     this.health = this.health - damage
+  }
+
+  _getSprite(): Sprites {
+    const health = 100 * this.health/this.maxHealth;
+    let index = Math.floor((100 - health)/30);
+    index = (index>=3) ? 3 : index;
+    return this.sprites[index];
   }
 
 
--- src/obects/Brick/pictureMap.ts
+++ src/obects/Brick/pictureMap.ts
@@ -1,7 +1,10 @@
 const brick = [
-  { x: 0 + 3 * 32, y: 0 + 1*32, width: 32, height: 32, }
-]
+  { x: 0 + 2 * 32 -1, y: 0 + 3*32, width: 32, height: 34, },
+];
 
+const brick70 = [{ x: 0 + 9 * 32 - 2, y: 0 + 8*32, width: 32, height: 34, }];
+const brick40 = [{ x: 0 + 10 * 32 - 1, y: 0 + 8*32, width: 32, height: 32, }];
+const brick10 = [{ x: 0 + 11 * 32 - 1, y: 0 + 8*32, width: 32, height: 32, }];
 export {
-  brick, 
+  brick,  brick70, brick40, brick10
 }
